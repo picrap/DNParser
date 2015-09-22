5423ee11d6263fc9e1ad1bd219b7f83cc4c03655
Pascal Craponne <pascal@craponne.fr>
2015-09-22 11:49:32 +0200

    YAT

--- CPI.DirectoryServices/Properties/AssemblyInfo.cs
+++ CPI.DirectoryServices/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("DNParser")]
 [assembly: AssemblyDescription("An RFC 2253 Compliant Distinguished Name Parser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pete Everett / Pascal Craponne")]
 [assembly: AssemblyProduct("DNParser")]
 [assembly: AssemblyCopyright("Written by Pete Everett (ported to Github and NuGet by Pascal Craponne) - CPOL License http://www.codeproject.com/info/cpol10.aspx")]
 [assembly: AssemblyTrademark("")]
