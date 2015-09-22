83eb28bc2ad7148bea5db9bd63fde9ad5aa56443
Pascal Craponne <pascal@craponne.fr>
2015-09-22 11:29:13 +0200

    SNK + correct assembly info

---
+++ CPI.DirectoryServices/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("DNParser")]
+[assembly: AssemblyDescription("An RFC 2253 Compliant Distinguished Name Parser")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DNParser")]
+[assembly: AssemblyCopyright("Written by Pete Everett (ported to Github and NuGet by Pascal Craponne) - CPOL License http://www.codeproject.com/info/cpol10.aspx")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0")]
